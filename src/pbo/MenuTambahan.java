c7848ba8eb7b697f2b74e11371a0ed07368287f5
TamaBarbar <TamaBarbar@DESKTOP-CB40O7Q.local>
2018-11-20 19:25:42 +0700

    remove comment test

---
+++ src/pbo/MenuTambahan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo;
+
+/**
+ *
+ * @author TamaBarbar
+ */
+public interface MenuTambahan {
+    public void tambah();
+    public void hapus();
+    public void tampil();
+}
