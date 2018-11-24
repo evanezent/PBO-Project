bfb16c2d5f664b6062fbaa608d1159b45546d5d8
Prana <pranapramudita@gmail.com>
2018-11-24 13:56:05 +0700

    Salah panggil loginPanitia malah di LoginPemilih

--- src/View/LoginPemilih.java
+++ src/View/LoginPemilih.java
@@ -178,8 +178,9 @@ public class LoginPemilih extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
-                new LoginPanitia().setVisible(true);
+                new LoginPemilih().setVisible(true);
             }
         });
     }
