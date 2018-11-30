c32fa9702fd57dfb1fe872e3f48b2986db77ac02
pranapramudita <pranapramudita@gmail.com>
2018-11-30 08:44:12 +0700

    Hapus Kandidat 3 dan 4, kan cuman 2 kandidatnya.

--- src/Controller/Controller_LC.java
+++ src/Controller/Controller_LC.java
@@ -28,7 +28,7 @@ public class Controller_LC extends MouseAdapter implements ActionListener {
         int nKandidat = kandidat.size()+1;
         int nPemilih = pemilih.size();
         double count;
-            String hasil; 
+        String hasil; 
         if (nKandidat == 2)
         {
             //no 1
@@ -39,18 +39,6 @@ public class Controller_LC extends MouseAdapter implements ActionListener {
             count = kandidat.get(1).getJumlahSuara()/nKandidat*100;
             hasil = Double.toString(count);
             view.setNo2(hasil);
-        }
-        if (nKandidat == 3)
-        {
-            count = kandidat.get(2).getJumlahSuara()/nKandidat*100;
-            hasil = Double.toString(count);
-            view.setNo3(hasil);
-        }
-        if (nKandidat== 4)
-        {
-            count = kandidat.get(3).getJumlahSuara()/nKandidat*100;
-            hasil = Double.toString(count);
-            view.setNo4(hasil);
         }
     }
        
