2622357fd493224e3f8943df958a9bc7092617c0
pratamays <pratamayo16@gmail.com>
2018-12-07 04:46:22 +0700

    SQL NEWEST

--- src/Database/OOP.sql
+++ src/Database/OOP.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 30, 2018 at 07:16 PM
+-- Generation Time: Dec 06, 2018 at 10:44 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
@@ -35,6 +35,14 @@ CREATE TABLE `Kandidat` (
   `jumlahSuara` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `Kandidat`
+--
+
+INSERT INTO `Kandidat` (`Ketua`, `Wakil`, `no_Urut`, `jumlahSuara`) VALUES
+('Jokowi', 'Maruf', '1', 4),
+('Prabowo', 'Sandy', '2', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -47,6 +55,17 @@ CREATE TABLE `Panitia` (
   `Password` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `Panitia`
+--
+
+INSERT INTO `Panitia` (`Nama Panitia`, `Username`, `Password`) VALUES
+('b', 'b', 'b'),
+('c', 'c', 'c'),
+('f', 'f', 'f'),
+('r', 'r', 'r'),
+('X', 'X', 'X');
+
 -- --------------------------------------------------------
 
 --
@@ -57,8 +76,20 @@ CREATE TABLE `Pemilih` (
   `Nama Pemilih` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `No_KTP` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `tgl_lahir` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `alamat` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL
+  `alamat` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `Pemilih`
+--
+
+INSERT INTO `Pemilih` (`Nama Pemilih`, `No_KTP`, `tgl_lahir`, `alamat`, `status`) VALUES
+('yusuf', '69', '1 januari 1999', 'njapol', 1),
+('tama', '777', '16 desember 99', 'pilangsari', 1),
+('a', 'a', 'a', 'a', 1),
+('b', 'b', 'b', 'b', 1),
+('q', 'q', 'q', 'q', 1);
 
 --
 -- Indexes for dumped tables
