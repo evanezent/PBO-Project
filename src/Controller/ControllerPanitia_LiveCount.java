39071018e4f1dd9a173264dbc150a21304e899e0
pranapramudita <pranapramudita@gmail.com>
2018-12-07 16:29:14 +0700

    Rapihin persenan

--- src/Controller/ControllerPanitia_LiveCount.java
+++ src/Controller/ControllerPanitia_LiveCount.java
@@ -9,6 +9,7 @@ import Model.*;
 import View.*;
 import java.util.*;
 import java.awt.event.*;
+import java.text.DecimalFormat;
 /**
  *
  * @author esber
@@ -35,10 +36,12 @@ public class ControllerPanitia_LiveCount extends MouseAdapter implements ActionL
         view.setVisible(true);
         
         count = kandidat.get(0).getHasilSuara()/nPemilih*100;
+        String.format("%.2f", count);
         hasil1 = Double.toString(count);
         view.setNo1(hasil1);
         //no 2
         count = kandidat.get(1).getHasilSuara()/nPemilih*100;
+        String.format("%.2f", count);
         hasil2 = Double.toString(count);
         view.setNo2(hasil2);
     }
